3285cf87d3c466c967b565aad86080c5c95d8a60
pistachiyoda <fujisawamai21@gmail.com>
2021-05-26 22:58:32 +0900

    色が範囲外の場合のエラーハンドリング

--- config/config.c
+++ config/config.c
@@ -110,14 +110,22 @@ int color_text_to_color_num(char *color_text)
 {
 	char **color_text_parts;
 	int color_num;
+	int i;
 
 	if (color_text == NULL)
 		end_game(1, "color text is null\n");
 
 	color_text_parts = ft_split(color_text, ',');
 
-	if (color_text_parts[0] == NULL || color_text_parts[1] == NULL || color_text_parts[2] == NULL)
-		end_game(1, "invalid color text format\n");
+	i = 0;
+	while (i < 3)
+	{
+		if (color_text_parts[i] == NULL)
+			end_game(1, "invalid color text format\n");
+		if (ft_atoi(color_text_parts[i]) < 0 || ft_atoi(color_text_parts[i]) > 255)
+			end_game(1, "color out of range\n");
+		i++;
+	}
 	// @todo ft_atoiに数字以外の文字列が渡るとエラーは返さず0になる
 	color_num = ft_atoi(color_text_parts[0]);
 	color_num = (color_num << 8) + ft_atoi(color_text_parts[1]);
