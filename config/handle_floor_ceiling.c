39a265cd5d76dcc7f8378a225b1e36a799a561dc
pistachiyoda <fujisawamai21@gmail.com>
2021-06-13 12:21:46 +0900

    カラーエレメントに数字以外が記述されていた場合のエラー処理を実装した

--- config/handle_floor_ceiling.c
+++ config/handle_floor_ceiling.c
@@ -1,17 +1,11 @@
 #include "cub3d.h"
+#include <stdio.h>
 
-/**
- * @todo ft_atoiに数字以外の文字列が渡るとエラーは返さず0になる
- */
-int	color_text_to_color_num(char *color_text)
+void	check_color(char **color_text_parts)
 {
-	char	**color_text_parts;
-	int		color_num;
-	int		i;
+	int	i;
+	int	j;
 
-	if (color_text == NULL)
-		end_game(1, "color text is null\n");
-	color_text_parts = ft_split(color_text, ',');
 	i = 0;
 	while (i < 3)
 	{
@@ -21,10 +15,28 @@ int	color_text_to_color_num(char *color_text)
 			ft_atoi(color_text_parts[i]) < 0
 			|| ft_atoi(color_text_parts[i]) > 255)
 			end_game(1, "color out of range\n");
+		j = 0;
+		while (color_text_parts[i][j])
+		{
+			if (!ft_isdigit(color_text_parts[i][j]))
+				end_game(1, "Error:Invalid color code");
+			j++;
+		}
 		i++;
 	}
 	if (color_text_parts[3] != NULL)
 		end_game(1, "Error:Too much color element.");
+}
+
+int	color_text_to_color_num(char *color_text)
+{
+	char	**color_text_parts;
+	int		color_num;
+
+	if (color_text == NULL)
+		end_game(1, "color text is null\n");
+	color_text_parts = ft_split(color_text, ',');
+	check_color(color_text_parts);
 	color_num = ft_atoi(color_text_parts[0]);
 	color_num = (color_num << 8) + ft_atoi(color_text_parts[1]);
 	color_num = (color_num << 8) + ft_atoi(color_text_parts[2]);
