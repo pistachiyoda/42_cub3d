0d10847dd234dbfe6e478514c3a9739bcde6d1e5
pistachiyoda <fujisawamai21@gmail.com>
2021-06-13 11:24:35 +0900

    カラーエレメントが４つ以上あったときのエラー処理を実装した

--- config/handle_floor_ceiling.c
+++ config/handle_floor_ceiling.c
@@ -23,6 +23,8 @@ int	color_text_to_color_num(char *color_text)
 			end_game(1, "color out of range\n");
 		i++;
 	}
+	if (color_text_parts[3] != NULL)
+		end_game(1, "Error:Too much color element.");
 	color_num = ft_atoi(color_text_parts[0]);
 	color_num = (color_num << 8) + ft_atoi(color_text_parts[1]);
 	color_num = (color_num << 8) + ft_atoi(color_text_parts[2]);
